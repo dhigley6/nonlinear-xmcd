c7444543af035aa40d0516ce4fb48426268e462f
dhigley6 <dhigley@stanford.edu>
2019-07-29 16:07:57 -0700

    fixed plotting behavior of Fig. 4a

--- code/visualize/quant_plot.py
+++ code/visualize/quant_plot.py
@@ -14,7 +14,7 @@ def make_figure():
     f, axs = plt.subplots(1, 2, sharex=True, figsize=(3.37, 2.4))
     quant = abs_get_processed_data.get_spec_quantification()
     dummy = np.array([0, 200])
-    axs[0].errorbar(quant['pulse_averaged_absorbed_energies'], quant['spec_energies'], marker='o', label='Expt.', yerr=quant['err_spec_energies'], xerr=0.2*quant['pulse_averaged_absorbed_energies'], linestyle='None', markerfacecolor='none', markeredgecolor='k', color='k', markeredgewidth=1, markersize=3, capsize=2)
+    axs[0].errorbar(quant['pulse_averaged_absorbed_energies'], quant['spec_energies'], marker='o', label='Expt.', yerr=quant['err_spec_energies'], xerr=0.2*quant['pulse_averaged_absorbed_energies'], linestyle='None', markerfacecolor='k', markeredgecolor='k', color='k', markeredgewidth=1, markersize=3, capsize=2)
     axs[0].plot(dummy, dummy*quant['fraction_within_2eV'], label=r'Fit, $\tau_c = 13$ fs', linestyle=(0, (3, 1.5)), color='r')
     axs[1].errorbar(quant['pulse_averaged_absorbed_energies'], quant['xmcds']*100, marker='o', label='Expt.', yerr=quant['err_xmcds']*100, xerr=0.2*quant['pulse_averaged_absorbed_energies'], linestyle='None', markerfacecolor='k', markeredgecolor='k', color='k', markeredgewidth=1, markersize=3, capsize=2)
     format_figure(f, axs)
